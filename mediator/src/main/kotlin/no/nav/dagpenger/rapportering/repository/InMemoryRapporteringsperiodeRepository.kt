779dd582a80c36a8fa63b1ca8328a4392d9251bb
André Roaldseth <andre.roaldseth@nav.no>
2023-06-01 11:38:52 +0200

    Lag en fallback rapporteringsperiode for å ikke brekke frontend

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/repository/InMemoryRapporteringsperiodeRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/repository/InMemoryRapporteringsperiodeRepository.kt
@@ -1,11 +1,11 @@
 package no.nav.dagpenger.rapportering.repository
 
 import no.nav.dagpenger.rapportering.Rapporteringsperiode
+import java.time.LocalDate
 import java.util.UUID
 
 class InMemoryRapporteringsperiodeRepository : RapporteringsperiodeRepository {
-
-//    ?: RapporteringsperiodeDTO(
+    //    ?: RapporteringsperiodeDTO(
 //    id = UUID.randomUUID(),
 //    fraOgMed = LocalDate.of(2023, 5, 22),
 //    tilOgMed = LocalDate.of(2023, 6, 4),
@@ -16,6 +16,7 @@ class InMemoryRapporteringsperiodeRepository : RapporteringsperiodeRepository {
     private val rapporteringsperioder = PersonCollection<Rapporteringsperiode>()
     override fun hentRapporteringsperiode(ident: String, uuid: UUID) =
         rapporteringsperioder.hent(ident).find { it.rapporteringsperiodeId == uuid }
+            ?: Rapporteringsperiode(LocalDate.now().minusDays(5))
 
     override fun hentRapporteringsperioder(ident: String) =
         rapporteringsperioder.hent(ident)
