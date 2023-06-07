f6e0b4209feba610973298ae11e260ea11582c32
celinetendam <celine.ten.dam@nav.no>
2023-06-07 13:42:39 +0200

    Utvider test ved å sjekke feltene i hendelsen
    Co-authored-by: Giao The Cung<giaothe@gmail.com>

--- mediator/src/test/kotlin/no/nav/dagpenger/rapportering/UtsendingsObserverTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/rapportering/UtsendingsObserverTest.kt
@@ -1,13 +1,17 @@
 package no.nav.dagpenger.rapportering
 
 import io.kotest.matchers.shouldBe
+import no.nav.dagpenger.rapportering.helpers.januar
 import no.nav.dagpenger.rapportering.hendelser.RapporteringsfristHendelse
 import no.nav.dagpenger.rapportering.tidslinje.Aktivitet
 import no.nav.dagpenger.rapportering.tidslinje.Dag
+import no.nav.helse.rapids_rivers.asLocalDate
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.util.UUID
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.hours
 
 class UtsendingsObserverTest {
 
@@ -23,14 +27,31 @@ class UtsendingsObserverTest {
         observer.rapporteringsperiodeInnsendt(
             RapporteringsperiodeObserver.RapporteringsperiodeInnsendt(
                 UUID.randomUUID(),
-                fom = LocalDate.now().minusDays(1),
-                tom = LocalDate.now().plusDays(13),
+                fom = 1.januar,
+                tom = 14.januar,
                 dager = listOf(
-                    Dag(LocalDate.now(), mutableListOf(Aktivitet.Arbeid(LocalDate.now(), 5))),
+                    Dag(5.januar, mutableListOf(Aktivitet.Arbeid(5.januar, 5))),
                 ),
             ),
         )
 
         testRapid.inspektør.size shouldBe 1
+        testRapid.inspektør.message(0).let {
+            it["@event_name"].asText() shouldBe "rapporteringsperiode_innsendt_hendelse"
+            it["ident"].asText() shouldBe "123"
+            it["fom"].asLocalDate() shouldBe 1.januar
+            it["tom"].asLocalDate() shouldBe 14.januar
+
+            it["dager"].let { dager ->
+                dager.size() shouldBe 1
+                dager.single()["dato"].asLocalDate() shouldBe 5.januar
+            }
+
+            it["dager"].single()["aktiviteter"].let { aktiviteter ->
+                aktiviteter.size() shouldBe 1
+                aktiviteter.single()["type"].asText() shouldBe "Arbeid"
+                Duration.parse(aktiviteter.single()["tid"].asText()) shouldBe 5.hours
+            }
+        }
     }
 }
