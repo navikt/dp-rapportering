0d2f77b18b2871f301b9389ca273d0d5c5cbbbc2
celinetendam <celine.ten.dam@nav.no>
2023-06-13 13:22:19 +0200

    Kjører jobben for ny rapporteringssyklus hver mandag

--- mediator/src/main/kotlin/no/nav/dagpenger/rapportering/NyRapporteringssyklusJobb.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/rapportering/NyRapporteringssyklusJobb.kt
@@ -2,24 +2,30 @@ package no.nav.dagpenger.rapportering
 
 import mu.KotlinLogging
 import no.nav.dagpenger.rapportering.hendelser.NyRapporteringssyklusHendelse
+import java.time.DayOfWeek
 import java.time.LocalDate
-import java.util.Calendar
-import java.util.Timer
-import java.util.TimerTask
+import java.time.LocalTime
+import java.time.ZonedDateTime
+import java.time.temporal.TemporalAdjusters
 import java.util.UUID
 import kotlin.concurrent.fixedRateTimer
-import kotlin.random.Random
 import kotlin.time.Duration.Companion.days
-import kotlin.time.Duration.Companion.minutes
 
 internal object NyRapporteringssyklusJobb {
     private val logger = KotlinLogging.logger {}
+    private val UKEDAG_FOR_KJØRING = DayOfWeek.MONDAY
+    private val TIDSPUNKT_FOR_KJØRING = LocalTime.of(15, 0)
+
+    private val nå = ZonedDateTime.now()
+    private val tidspunktForNesteKjøring = nå.with(TemporalAdjusters.nextOrSame(UKEDAG_FOR_KJØRING)).with(TIDSPUNKT_FOR_KJØRING)
+    private val millisekunderTilNesteKjøring = tidspunktForNesteKjøring.toInstant().toEpochMilli() - nå.toInstant().toEpochMilli() // differansen i millisekunder mellom de to tidspunktene
+
     internal fun start(mediator: Mediator) {
         fixedRateTimer(
-            name = "Fast opprettelse av nye rapporteringssyklus",
+            name = "Fast opprettelse av ny rapporteringssyklus",
             daemon = true,
-            initialDelay = randomInitialDelay(),
-            period = 15.minutes.inWholeMilliseconds,
+            initialDelay = millisekunderTilNesteKjøring.coerceAtLeast(0), // Har nåværende tidspunkt passert 'tidspunktForNesteKjøring' starter vi timeren umiddelbart
+            period = 7.days.inWholeMilliseconds,
             action = {
                 try {
                     logger.info { "Starter opprettelse av ny rapporteringssyklus" }
@@ -38,40 +44,4 @@ internal object NyRapporteringssyklusJobb {
             behandle(NyRapporteringssyklusHendelse(UUID.randomUUID(), ident, LocalDate.now()))
         }
     }
-
-    private fun randomInitialDelay() = Random.nextInt(10).minutes.inWholeMilliseconds
-}
-
-fun main() {
-    val timer = Timer()
-    // Get current date and time
-    val currentTime = Calendar.getInstance()
-    // Set the time to Monday at 15:00
-    currentTime.set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
-    currentTime.set(Calendar.HOUR_OF_DAY, 15)
-    currentTime.set(Calendar.MINUTE, 0)
-    currentTime.set(Calendar.SECOND, 0)
-    // Calculate the delay until the next Monday at 15:00
-    val delay = currentTime.timeInMillis - System.currentTimeMillis()
-    // Schedule the task to run every week on Monday at 15:00
-    timer.scheduleAtFixedRate(
-        /* task = */
-        object : TimerTask() {
-            override fun run() {
-                // Call your Kotlin function here
-                yourFunction()
-            }
-        },
-        /* delay = */
-        delay,
-        /* period = */
-        7.days.inWholeMilliseconds,
-    ) // Run every 7 days
-    // Keep the main thread running to allow the scheduled task to execute
-    Thread.currentThread().join()
-}
-
-fun yourFunction() {
-    // Your function code here
-    println("Executing yourFunction() at ${Calendar.getInstance().time}")
 }
