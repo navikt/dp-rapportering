4b06890a89f749fcdaea1ff706e4bfdc67ffb5de
André Roaldseth <andre.roaldseth@nav.no>
2023-06-22 15:44:14 +0200

    Lag en test som verifiserer at vi tar i mot rapporteringspliktdatohendelser

---
+++ mediator/src/test/kotlin/no/nav/dagpenger/rapportering/tjenester/RapporteringspliktDatoMottakTest.kt
@@ -0,0 +1,51 @@
+package no.nav.dagpenger.rapportering.tjenester
+
+import io.mockk.mockk
+import io.mockk.verify
+import no.nav.dagpenger.rapportering.IHendelseMediator
+import no.nav.dagpenger.rapportering.hendelser.RapporteringspliktDatoHendelse
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.intellij.lang.annotations.Language
+import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.UUID
+
+class RapporteringspliktDatoMottakTest {
+    private val rapid = TestRapid()
+    private val mediator = mockk<IHendelseMediator>(relaxed = true)
+    private val mottak = RapporteringspliktDatoMottak(rapid, mediator)
+
+    @Test
+    fun foobar() {
+        rapid.sendTestMessage(løstBehovJSON)
+
+        verify {
+            mediator.behandle(any<RapporteringspliktDatoHendelse>())
+        }
+    }
+}
+
+@Language("JSON")
+private val løstBehovJSON = """
+    {
+      "@id": "${UUID.randomUUID()}",
+      "@event_name": "behov",
+      "@behov": [
+        "Virkningsdatoer",
+        "Søknadstidspunkt"
+      ],
+      "@opprettet": "${LocalDateTime.now()}",
+      "ident": "123",
+      "søknad_uuid": "${UUID.randomUUID()}",
+      "@løsning": {
+        "Virkningsdatoer": {
+          "ønsketdato": "${LocalDate.now()}"
+        },
+        "Søknadstidspunkt": "${LocalDate.now()}"
+      },
+      "@ferdig": true
+    }
+    
+    
+""".trimIndent()
