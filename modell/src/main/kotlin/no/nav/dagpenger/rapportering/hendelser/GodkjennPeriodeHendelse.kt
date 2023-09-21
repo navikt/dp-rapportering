1beccef925c46b79f77f5f347e525ad2ebfde524
celinetendam <celine.ten.dam@nav.no>
2023-09-21 10:30:50 +0200

    Legger til dato felt på GodkjennPeriodeHendelse

--- modell/src/main/kotlin/no/nav/dagpenger/rapportering/hendelser/GodkjennPeriodeHendelse.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/rapportering/hendelser/GodkjennPeriodeHendelse.kt
@@ -4,6 +4,7 @@ import no.nav.dagpenger.aktivitetslogg.Aktivitetslogg
 import no.nav.dagpenger.rapportering.Godkjenningsendring
 import no.nav.dagpenger.rapportering.Godkjenningsendring.Saksbehandler
 import no.nav.dagpenger.rapportering.Godkjenningsendring.Sluttbruker
+import java.time.LocalDate
 import java.util.UUID
 
 class GodkjennPeriodeHendelse(
@@ -11,22 +12,31 @@ class GodkjennPeriodeHendelse(
     ident: String,
     val rapporteringsperiodeId: UUID,
     val godkjenningsendring: Godkjenningsendring,
+    val dato: LocalDate,
 ) : PersonHendelse(
     meldingsreferanseId,
     ident,
     Aktivitetslogg(),
 ) {
-    constructor(ident: String, rapporteringId: UUID) : this(
-        UUID.randomUUID(),
-        ident,
-        rapporteringId,
-        Godkjenningsendring(Sluttbruker(ident)),
+    constructor(ident: String, rapporteringId: UUID, dato: LocalDate = LocalDate.now()) : this(
+        meldingsreferanseId = UUID.randomUUID(),
+        ident = ident,
+        rapporteringsperiodeId = rapporteringId,
+        godkjenningsendring = Godkjenningsendring(Sluttbruker(ident)),
+        dato = dato,
     )
 
-    constructor(ident: String, rapporteringId: UUID, kilde: Saksbehandler, begrunnelse: String) : this(
-        UUID.randomUUID(),
-        ident,
-        rapporteringId,
-        Godkjenningsendring(kilde, begrunnelse),
+    constructor(
+        ident: String,
+        rapporteringId: UUID,
+        kilde: Saksbehandler,
+        begrunnelse: String,
+        dato: LocalDate = LocalDate.now(),
+    ) : this(
+        meldingsreferanseId = UUID.randomUUID(),
+        ident = ident,
+        rapporteringsperiodeId = rapporteringId,
+        godkjenningsendring = Godkjenningsendring(kilde, begrunnelse),
+        dato = dato,
     )
 }
