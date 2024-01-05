22385eb2aaea96c9de32b99f8e4922bc35680b57
Abdifatah Bashi <abdifatah.bashi@nav.no>
2024-01-05 15:26:22 +0100

    Rafaktorere og forbedre testen til Aktivitetstidslinje

--- modell/src/test/kotlin/no/nav/dagpenger/rapportering/AktivitetstidslinjeTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/rapportering/AktivitetstidslinjeTest.kt
@@ -1,42 +1,66 @@
 package no.nav.dagpenger.rapportering
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.rapportering.helpers.januar
 import no.nav.dagpenger.rapportering.tidslinje.Aktivitet.Arbeid
 import no.nav.dagpenger.rapportering.tidslinje.Aktivitet.Ferie
 import no.nav.dagpenger.rapportering.tidslinje.Aktivitet.Syk
 import no.nav.dagpenger.rapportering.tidslinje.Aktivitetstidslinje
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
 
 class AktivitetstidslinjeTest {
     @Test
-    fun `tidslinje holder på dager, fritak og aktiviteter`() {
+    fun `tidslinje har riktig antall dager`() {
+        val periode = 1.januar..14.januar
+
+        val tidslinje = Aktivitetstidslinje(periode)
+
+        tidslinje.size shouldBe 14
+        tidslinje.dagerMedAktivitet shouldBe 0
+    }
+
+    @Test
+    fun `kan ikke rapportere aktivitet på dager uten rapporteringsplikt`() {
         val periode = 1.januar..14.januar
         val tidslinje = Aktivitetstidslinje(periode)
-        // Tidslinjen skal alltid ha like mange dager som perioden
-        assertEquals(14, tidslinje.size)
-        // Legg til dager uten rapporteringsplikt
-        tidslinje.leggTilFritak(1.januar)
-        tidslinje.leggTilFritak(2.januar)
-        tidslinje.leggTilFritak(8.januar, 9.januar, 10.januar)
-        // Kan ikke rapportere aktivitet på dager uten rapporteringsplikt
-        assertThrows<IllegalStateException> {
-            tidslinje.leggTilAktivitet(Arbeid(8.januar, "PT3H"))
-        }
-        // Legg til aktiviteter
-        tidslinje.leggTilAktivitet(Arbeid(3.januar, "PT3H20M"))
-        tidslinje.leggTilAktivitet(Arbeid(4.januar, "PT2H30M"))
-        tidslinje.leggTilAktivitet(Syk(5.januar))
-        tidslinje.leggTilAktivitet(Ferie(6.januar))
+        val fritaksDager = listOf(1.januar, 2.januar, 3.januar)
 
-        assertEquals(14, tidslinje.size)
-        assertEquals(4, tidslinje.dagerMedAktivitet)
+        tidslinje.leggTilFritak(*fritaksDager.toTypedArray())
 
-        tidslinje.forEach {
-            // Alle dager i tidslinja er dekket av perioden
-            assertTrue(it.dekkesAv(periode))
+        fritaksDager.forEach {
+            shouldThrow<IllegalStateException> { tidslinje.leggTilAktivitet(Arbeid(it, "PT3H")) }
         }
+    }
+
+    @Test
+    fun `legge til aktiviteter`() {
+        val periode = 1.januar..14.januar
+        val tidslinje = Aktivitetstidslinje(periode)
+
+        tidslinje.apply {
+            leggTilAktivitet(Arbeid(3.januar, "PT3H20M"))
+            leggTilAktivitet(Arbeid(4.januar, "PT2H30M"))
+            leggTilAktivitet(Syk(5.januar))
+            leggTilAktivitet(Ferie(6.januar))
+        }
+
+        tidslinje.dagerMedAktivitet shouldBe 4
+        tidslinje.size shouldBe 14
+    }
+
+    @Test
+    fun `alle dager i tidslinja er dekket av perioden`() {
+        val periode = 1.januar..14.januar
+        val tidslinje = Aktivitetstidslinje(periode)
+
+        tidslinje.apply {
+            leggTilFritak(1.januar)
+            leggTilAktivitet(Arbeid(2.januar, "PT3H20M"))
+            leggTilAktivitet(Syk(5.januar))
+            leggTilAktivitet(Ferie(6.januar))
+        }
+
+        tidslinje.all { it.dekkesAv(periode) } shouldBe true
     }
 }
