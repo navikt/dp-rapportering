c291b3083f54faf02fee645c6532e33c8edebec4
Abdifatah Bashi <abdifatah.bashi@nav.no>
2024-01-08 15:17:43 +0100

    Rafaktorere og legge til tester for Godkjenningslogg
    Co-authored-by: Celine ten Dam <celine.ten.dam@nav.no>

--- modell/src/test/kotlin/no/nav/dagpenger/rapportering/GodkjenningsloggTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/rapportering/GodkjenningsloggTest.kt
@@ -8,22 +8,51 @@ import org.junit.jupiter.api.assertThrows
 
 class GodkjenningsloggTest {
     @Test
-    fun foo() {
+    fun `skal ikke være godkjent initialt`() {
         val godkjenninger = Godkjenningslogg()
+
         godkjenninger.godkjent() shouldBe false
+    }
+
+    @Test
+    fun `skal være godkjent etter å ha lagt til Godkjenningsendring`() {
+        val godkjenninger = Godkjenningslogg()
 
         godkjenninger.leggTil(Godkjenningsendring(Sluttbruker("123")))
+
         godkjenninger.godkjent() shouldBe true
+    }
+
+    @Test
+    fun `kan ikke avgodkjenne uten å være godkjent`() {
+        val godkjenninger = Godkjenningslogg()
 
         assertThrows<IllegalArgumentException> {
-            godkjenninger.leggTil(Godkjenningsendring(Saksbehandler("123"), "foo"))
+            godkjenninger.avgodkjenn(Godkjenningsendring(Saksbehandler("123"), ""))
+        }
+    }
+
+    @Test
+    fun `kan avgodkjenne etter å ha blitt godkjent`() {
+        val godkjenninger = Godkjenningslogg()
+
+        with(godkjenninger) {
+            leggTil(Godkjenningsendring(Sluttbruker("123")))
+            avgodkjenn(Godkjenningsendring(Saksbehandler("123"), ""))
         }
 
-        godkjenninger.avgodkjenn(Godkjenningsendring(Saksbehandler("123"), "bar"))
         godkjenninger.godkjent() shouldBe false
+    }
 
-        assertThrows<IllegalArgumentException> {
-            godkjenninger.avgodkjenn(Godkjenningsendring(Saksbehandler("123"), "foobar"))
+    @Test
+    fun `kan bare endre dersom det er samme kilde`() {
+        val saksbehandler = Saksbehandler("saksbehandler1")
+        val sluttbruker = Sluttbruker("bruker1")
+        val endring = Godkjenningsendring(sluttbruker)
+
+        with(endring) {
+            kanEndre(saksbehandler) shouldBe false
+            kanEndre(sluttbruker) shouldBe true
         }
     }
 }
