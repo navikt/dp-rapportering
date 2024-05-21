7148acfb3853043192eb280af4ea5b11db2cd3a7
Abdifatah Bashi <abdifatah.bashi@nav.no>
2024-05-21 14:41:49 +0200

    Fjern repository-mappen

--- src/main/kotlin/no/nav/dagpenger/rapportering/repository/RapporteringsRepository.kt
+++
@@ -1,7 +0,0 @@
-package no.nav.dagpenger.rapportering.repository
-
-import no.nav.dagpenger.rapportering.model.Rapporteringsperiode
-
-interface RapporteringsRepository {
-    fun hentRapporteringsperioder(ident: String): List<Rapporteringsperiode>
-}
