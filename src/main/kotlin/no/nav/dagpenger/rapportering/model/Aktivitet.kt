f4ffa108c46393401115cdd6a93dbd3349341a7a
Abdifatah Bashi <abdifatah.bashi@nav.no>
2024-05-22 09:28:02 +0200

    Oppdater openapi-spec for frontend api

--- src/main/kotlin/no/nav/dagpenger/rapportering/model/Aktivitet.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/model/Aktivitet.kt
@@ -1,8 +1,10 @@
 package no.nav.dagpenger.rapportering.model
 
 import java.time.LocalDate
+import java.util.UUID
 
 data class Aktivitet(
+    val uuid: UUID,
     val dato: LocalDate,
     val type: AktivitetsType,
 )
