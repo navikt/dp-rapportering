d40df8d3da4da2046d9a5ad48ecbad816a61e263
Igor Weber <igor.shuliakov@nav.no>
2024-06-11 10:52:30 +0200

    Journalpost og JournalpostResponse

---
+++ src/main/kotlin/no/nav/dagpenger/rapportering/model/JournalpostResponse.kt
@@ -0,0 +1,20 @@
+package no.nav.sbl.meldekort.model.meldekort.journalpost
+
+import com.fasterxml.jackson.annotation.JsonInclude
+
+// Se https://confluence.adeo.no/display/BOA/opprettJournalpost
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class JournalpostResponse(
+    // Dokumentasjon sier at dette feltet er String. Men det ser ut at vi får numerisk ID her
+    val journalpostId: Long,
+    val journalstatus: String,
+    val melding: String? = null,
+    val journalpostferdigstilt: Boolean,
+    val dokumenter: List<DokumentInfo>,
+)
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+data class DokumentInfo(
+    // Dokumentasjon sier at dette feltet er String. Men det ser ut at vi får numerisk ID her
+    val dokumentInfoId: Long,
+)
