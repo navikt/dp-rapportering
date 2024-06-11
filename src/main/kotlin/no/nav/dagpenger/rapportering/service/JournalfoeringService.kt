e7e3df2f9c83f0ae720ccbf6e790ae83aaca9a14
Igor Weber <igor.shuliakov@nav.no>
2024-06-11 14:12:36 +0200

    JournalfoeringService

---
+++ src/main/kotlin/no/nav/dagpenger/rapportering/service/JournalfoeringService.kt
@@ -0,0 +1,8 @@
+package no.nav.dagpenger.rapportering.service
+
+import no.nav.dagpenger.rapportering.model.Rapporteringsperiode
+
+class JournalfoeringService {
+    suspend fun journalfoer(rapporteringsperiode: Rapporteringsperiode) {
+    }
+}
