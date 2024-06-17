b3a4d5bee9564a6fb7639ad57618096da025cfe2
Igor Weber <igor.shuliakov@nav.no>
2024-06-17 12:06:58 +0200

    SQL for journalføring

---
+++ src/main/resources/db/migration/V2__CREATE_TABLES_JOURNALFOERING.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS opprettede_journalposter
+(
+    journalpost_id  BIGINT                              PRIMARY KEY,
+    rapportering_id BIGINT                              NOT NULL REFERENCES rapporteringsperiode(id),
+    dokumentinfo_id BIGINT                              NOT NULL,
+    created         TIMESTAMP DEFAULT current_timestamp NOT NULL
+);
+
+CREATE INDEX  IF NOT EXISTS opprettede_journalposter_rapportering_id_index ON opprettede_journalposter (rapportering_id);
+
+CREATE TABLE IF NOT EXISTS midlertidig_lagrede_journalposter
+(
+    id              CHAR(36)                            PRIMARY KEY,
+    journalpost     TEXT                                NOT NULL,
+    created         TIMESTAMP DEFAULT current_timestamp NOT NULL,
+    retries         INT       DEFAULT 0                 NOT NULL
+);
