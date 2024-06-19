7c4f934541a7217dca75d4ef910af17e46c46e42
Igor Weber <igor.shuliakov@nav.no>
2024-06-19 08:41:57 +0200

    URL logg

--- src/main/kotlin/no/nav/dagpenger/rapportering/service/JournalfoeringService.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/service/JournalfoeringService.kt
@@ -8,11 +8,10 @@ import io.ktor.client.call.body
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.request.accept
-import io.ktor.client.request.header
+import io.ktor.client.request.bearerAuth
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
-import io.ktor.http.HttpHeaders
 import io.ktor.http.contentType
 import kotlinx.coroutines.runBlocking
 import mu.KLogging
@@ -208,11 +207,12 @@ class JournalfoeringService(
             val token = tokenProvider.invoke("api://${Configuration.dokarkivAudience}/.default")
 
             logger.info("Prøver å sende journalpost " + journalpost.eksternReferanseId)
+            logger.info("URL: $dokarkivUrl$path")
 
             val response =
                 httpClient
                     .post(URI("$dokarkivUrl$path").toURL()) {
-                        header(HttpHeaders.Authorization, "Bearer $token")
+                        bearerAuth(token)
                         accept(ContentType.Application.Json)
                         contentType(ContentType.Application.Json)
                         setBody(journalpost)
