357c2fb61a99f75d65a0a1052666a4e5984d5261
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2024-07-03 13:19:58 +0200

    Legger til kotlinModule for content negotiation med json for HttpClient

--- src/main/kotlin/no/nav/dagpenger/rapportering/connector/HttpClient.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/connector/HttpClient.kt
@@ -3,6 +3,7 @@ package no.nav.dagpenger.rapportering.connector
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
@@ -23,6 +24,7 @@ fun createHttpClient(engine: HttpClientEngine) =
 
         install(ContentNegotiation) {
             jackson {
+                registerKotlinModule()
                 registerModule(JavaTimeModule())
                 disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
                 configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
