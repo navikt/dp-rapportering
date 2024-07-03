a4acbdcc0a75f50363bc498c23ef25343e7496ca
Igor Weber <igor.shuliakov@nav.no>
2024-07-03 09:38:11 +0200

    optionalIdent fra call

--- src/main/kotlin/no/nav/dagpenger/rapportering/utils/IncomingCallLoggingPlugin.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/utils/IncomingCallLoggingPlugin.kt
@@ -22,7 +22,7 @@ import kotlinx.coroutines.runBlocking
 import mu.KotlinLogging
 import no.nav.dagpenger.rapportering.Configuration.MDC_CORRELATION_ID
 import no.nav.dagpenger.rapportering.Configuration.NO_LOG_PATHS
-import no.nav.dagpenger.rapportering.api.auth.ident
+import no.nav.dagpenger.rapportering.api.auth.optionalIdent
 import no.nav.dagpenger.rapportering.model.KallLogg
 import no.nav.dagpenger.rapportering.repository.KallLoggRepository
 import org.slf4j.MDC
@@ -43,11 +43,7 @@ val IncomingCallLoggingPlugin: ApplicationPlugin<ICDLPConfig> =
                 return@onCall
             }
 
-            var ident = ""
-            try {
-                ident = call.ident()
-            } catch (_: Exception) {
-            }
+            val ident = call.optionalIdent() ?: ""
 
             val requestData =
                 StringBuilder()
