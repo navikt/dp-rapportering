3237890602bdffa995eb84c96dbfadc110522ef2
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2024-07-16 12:43:47 +0200

    Fjerner httpClient-variabel i main

--- src/main/kotlin/no/nav/dagpenger/rapportering/Application.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/Application.kt
@@ -1,31 +1,15 @@
 package no.nav.dagpenger.rapportering
 
-import io.ktor.client.HttpClient
 import io.ktor.client.engine.cio.CIO
-import io.ktor.server.application.Application
-import no.nav.dagpenger.rapportering.api.internalApi
-import no.nav.dagpenger.rapportering.api.rapporteringApi
 import no.nav.dagpenger.rapportering.config.Configuration
-import no.nav.dagpenger.rapportering.config.Configuration.appMicrometerRegistry
-import no.nav.dagpenger.rapportering.config.konfigurasjon
-import no.nav.dagpenger.rapportering.connector.DokarkivConnector
-import no.nav.dagpenger.rapportering.connector.MeldepliktConnector
 import no.nav.dagpenger.rapportering.connector.createHttpClient
-import no.nav.dagpenger.rapportering.jobs.RapporterDatabaseMetrikkerJob
-import no.nav.dagpenger.rapportering.jobs.SlettRapporteringsperioderJob
-import no.nav.dagpenger.rapportering.repository.JournalfoeringRepositoryPostgres
-import no.nav.dagpenger.rapportering.repository.PostgresDataSourceBuilder.dataSource
-import no.nav.dagpenger.rapportering.repository.PostgresDataSourceBuilder.preparePartitions
-import no.nav.dagpenger.rapportering.repository.RapporteringRepositoryPostgres
-import no.nav.dagpenger.rapportering.service.JournalfoeringService
-import no.nav.dagpenger.rapportering.service.RapporteringService
 
-fun main(httpClient: HttpClient = createHttpClient(CIO.create {})) {
+fun main() {
     // embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module).start(wait = true)
-    ApplicationBuilder(Configuration.config, httpClient).start()
+    ApplicationBuilder(Configuration.config, createHttpClient(CIO.create {})).start()
 }
 
-fun Application.module(httpClient: HttpClient = createHttpClient(CIO.create {})) {
+/* fun Application.module(httpClient: HttpClient = createHttpClient(CIO.create {})) {
     preparePartitions()
 
     val meldepliktConnector = MeldepliktConnector(httpClient = httpClient)
@@ -48,4 +32,4 @@ fun Application.module(httpClient: HttpClient = createHttpClient(CIO.create {}))
 
     SlettRapporteringsperioderJob.start(rapporteringService)
     RapporterDatabaseMetrikkerJob.start(rapporteringRepository, journalfoeringRepository)
-}
+} */
