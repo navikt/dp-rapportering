bf9e4fdd6e97dfee5e6a1f1f17d821bdb80ba3a0
Yrjan Fraschetti <yrjan.fraschetti@bekk.no>
2024-10-04 12:53:52 +0200

    Endrer filtrering i kobleRapporteringsperioder til å sjekke at original periode ikke har begrunnelse

--- src/main/kotlin/no/nav/dagpenger/rapportering/service/RapporteringService.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/service/RapporteringService.kt
@@ -185,7 +185,7 @@ class RapporteringService(
                     if (perioder.size > 1) {
                         val originalPeriode =
                             perioder
-                                .filter { it.status == Endret }
+                                .filter { it.begrunnelseEndring == null }
                                 .minByOrNull {
                                     it.mottattDato ?: throw RuntimeException("Innsendt periode har ikke mottatt dato")
                                 } ?: throw RuntimeException("Fant ingen original periode")
