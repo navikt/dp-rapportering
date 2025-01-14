f5dfe100a5c442df2e457dd83061255e1c04debf
Igor Weber <igor.shuliakov@nav.no>
2025-01-14 15:30:59 +0100

    maximumPoolSize = 20

--- src/main/kotlin/no/nav/dagpenger/rapportering/repository/PostgresDataSourceBuilder.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/repository/PostgresDataSourceBuilder.kt
@@ -20,7 +20,7 @@ internal object PostgresDataSourceBuilder {
     val dataSource by lazy {
         HikariDataSource().apply {
             jdbcUrl = getOrThrow(DB_URL_KEY) + "&tcpKeepAlive=true"
-            maximumPoolSize = 10
+            maximumPoolSize = 20
             minimumIdle = 1
             idleTimeout = 10000 // 10s
             connectionTimeout = 5000 // 5s
