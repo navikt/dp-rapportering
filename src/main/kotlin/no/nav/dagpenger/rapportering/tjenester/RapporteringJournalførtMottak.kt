4a5b5f928136ed157316bba726f2a711fcfca5db
Igor Weber <85676385+igorweber@users.noreply.github.com>
2026-01-19 19:50:13 +0100

    Fjernet onError fra RapporteringJournalført

--- src/main/kotlin/no/nav/dagpenger/rapportering/tjenester/RapporteringJournalførtMottak.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/tjenester/RapporteringJournalførtMottak.kt
@@ -4,7 +4,6 @@ import com.github.navikt.tbd_libs.rapids_and_rivers.JsonMessage
 import com.github.navikt.tbd_libs.rapids_and_rivers.River
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageContext
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageMetadata
-import com.github.navikt.tbd_libs.rapids_and_rivers_api.MessageProblems
 import com.github.navikt.tbd_libs.rapids_and_rivers_api.RapidsConnection
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.github.oshai.kotlinlogging.withLoggingContext
@@ -78,13 +77,5 @@ internal class RapporteringJournalførtMottak(
                 kallLoggRepository.lagreResponse(kallLoggId, 200, packet.toJson())
             }
         }
-    }
-
-    override fun onError(
-        problems: MessageProblems,
-        context: MessageContext,
-        metadata: MessageMetadata,
-    ) {
-        logger.info { problems.toExtendedReport() }
     }
 }
