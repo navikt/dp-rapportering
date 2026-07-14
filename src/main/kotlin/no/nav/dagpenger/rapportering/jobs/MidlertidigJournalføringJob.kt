8de5aec55fa84998c8ecfb0781e0f12f12c2f9ed
André Roaldseth <andre.roaldseth@nav.no>
2026-07-14 10:32:01 +0200

    Legg på tracing av MidlertidigJournalføringJob

--- src/main/kotlin/no/nav/dagpenger/rapportering/jobs/MidlertidigJournalføringJob.kt
+++ src/main/kotlin/no/nav/dagpenger/rapportering/jobs/MidlertidigJournalføringJob.kt
@@ -2,12 +2,15 @@ package no.nav.dagpenger.rapportering.jobs
 
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.HttpClient
+import io.opentelemetry.api.GlobalOpenTelemetry
 import kotlinx.coroutines.runBlocking
 import no.nav.dagpenger.rapportering.metrics.JobbkjøringMetrikker
 import no.nav.dagpenger.rapportering.service.JournalfoeringService
 import java.util.Timer
 import java.util.TimerTask
+import kotlin.jvm.java
 import kotlin.time.measureTime
+import kotlin.use
 
 internal class MidlertidigJournalføringJob(
     private val httpClient: HttpClient,
@@ -22,25 +25,34 @@ internal class MidlertidigJournalføringJob(
         val timerTask: TimerTask =
             object : TimerTask() {
                 override fun run() {
+                    val span = tracer.spanBuilder("midlertidig-journalføring").startSpan()
                     try {
-                        if (isLeader(httpClient, logger)) {
-                            logger.info { "Pod er leader. Starter jobb for å sende journalposter på nytt" }
-                            var rowsAffected: Int
-                            val tidBrukt =
-                                measureTime {
-                                    rowsAffected = runBlocking { journalføringService.journalfoerPaaNytt() }
-                                }
-                            jobbkjøringMetrikker.jobbFullfort(tidBrukt, rowsAffected)
+                        span.makeCurrent().use {
+                            if (isLeader(httpClient, logger)) {
+                                logger.info { "Pod er leader. Starter jobb for å sende journalposter på nytt" }
+                                var rowsAffected: Int
+                                val tidBrukt =
+                                    measureTime {
+                                        rowsAffected = runBlocking { journalføringService.journalfoerPaaNytt() }
+                                    }
+                                jobbkjøringMetrikker.jobbFullfort(tidBrukt, rowsAffected)
+                            }
                         }
                     } catch (e: Exception) {
+                        span.recordException(e)
                         logger.error(e) { "JournalfoerPaaNytt feilet" }
                         jobbkjøringMetrikker.jobbFeilet()
                     } finally {
                         jobbkjøringMetrikker.jobbSjekketOmDenSkulleKjøre()
+                        span.end()
                     }
                 }
             }
 
         timer.schedule(timerTask, delay, resendInterval)
+    }
+
+    private companion object {
+        private val tracer = GlobalOpenTelemetry.getTracer(MidlertidigJournalføringJob::class.java.name)
     }
 }
